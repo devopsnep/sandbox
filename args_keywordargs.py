27a0336dbb4c5875653410d0de4f2f3cc72df75a
urgen sherpa <norbuurgen@gmail.com>
2017-10-22 21:44:14 +0545

    Update args_keywordargs.py

--- args_keywordargs.py
+++ args_keywordargs.py
@@ -1,6 +1,3 @@
-'''
-    using arguments,multiple arguments and keyword arguments in python functions
-'''
 #!/usr/bin/python3
 def main():
     combinedFunc(22,43,55,"jack",78,37,lname="sherpa",fname="orange",address="kathmandu")
@@ -9,5 +6,8 @@ def combinedFunc(a,b,c, *args, **kwargs):
     print("individual args: ",a,b,c)
     print("list args: ",args)
     print("keyword args: ",kwargs)
+    for k in kwargs:
+        print("key {} => {}". format(k,kwargs[k]))
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
